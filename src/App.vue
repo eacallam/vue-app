c53631e588db164e826d9e16fd5eb13af07ab3cf
eacallam <bethcallam2@gmail.com>
2019-10-22 20:33:22 -0400

    Add ability to delete employees

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
     <employee-form @add:employee="addEmployee" />
-    <employee-table :employees="employees" />
+    <employee-table :employees="employees" @delete:employee="deleteEmployee" />
   </div>
 </template>
 
@@ -48,7 +48,12 @@
         const newEmployee = { ...employee, id };
 
         this.employees = [...this.employees, newEmployee]
-      }
+      },
+      deleteEmployee(id) {
+        this.employees = this.employees.filter(
+          employee => employee.id !== id
+      )
+     }
     }
   }
 </script>
