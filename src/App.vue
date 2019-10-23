2d955d2692d8dd424cb6c5d4aacd27258763423e
eacallam <bethcallam2@gmail.com>
2019-10-23 09:21:01 -0400

    Add ability to edit employees

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
     <employee-form @add:employee="addEmployee" />
-    <employee-table :employees="employees" @delete:employee="deleteEmployee" />
+    <employee-table
+    :employees="employees"
+    @delete:employee="deleteEmployee"
+    @edit:employee="editEmployee"
+  />
   </div>
 </template>
 
@@ -52,8 +56,13 @@
       deleteEmployee(id) {
         this.employees = this.employees.filter(
           employee => employee.id !== id
-      )
-     }
+        )
+      },
+      editEmployee(id, updatedEmployee) {
+        this.employees = this.employees.map(employee =>
+          employee.id === id ? updatedEmployee : employee
+        )
+      }
     }
   }
 </script>
