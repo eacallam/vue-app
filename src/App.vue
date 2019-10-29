3bd50fe986bb0ccc5e5e6eae7b395529795b3465
eacallam <bethcallam2@gmail.com>
2019-10-29 07:43:42 -0400

    Implement dummy asynchronous API calls

--- src/App.vue
+++ src/App.vue
@@ -22,46 +22,74 @@
     },
     data() {
       return {
-        employees: [
-          {
-            id: 1,
-            name: 'Richard Hendricks',
-            email: 'richard@piedpiper.com',
-          },
-          {
-            id: 2,
-            name: 'Bertram Gilfoyle',
-            email: 'gilfoyle@piedpiper.com',
-          },
-          {
-            id: 3,
-            name: 'Dinesh Chugtai',
-            email: 'dinesh@piedpiper.com',
-          },
-        ],
+        employees: [],
       }
     },
-    methods: {
-      addEmployee(employee) {
-        // code to generate an id since this is not connected to a DB
-        const lastId =
-          this.employees.length > 0
-            ? this.employees[this.employees.length - 1].id
-            : 0;
-        const id = lastId + 1;
-        const newEmployee = { ...employee, id };
 
-        this.employees = [...this.employees, newEmployee]
+    mounted() {
+      this.getEmployees()
+    },
+
+    methods: {
+      async getEmployees() {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users')
+          const data = await response.json()
+          this.employees = data
+        } catch (error) { // eslint-disable-next-line no-console
+          console.error(error);
+        }
       },
-      deleteEmployee(id) {
-        this.employees = this.employees.filter(
-          employee => employee.id !== id
-        )
+      async addEmployee(employee) {
+        // code to generate an id since this is not connected to a DB
+        // const lastId =
+        //   this.employees.length > 0
+        //     ? this.employees[this.employees.length - 1].id
+        //     : 0;
+        // const id = lastId + 1;
+        // const newEmployee = { ...employee, id };
+
+        // this.employees = [...this.employees, newEmployee]
+
+        // Implement asynchronous API calls instead:
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: JSON.stringify(employee),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' }
+          })
+          const data = await response.json()
+          this.employees = [...this.employees, data]
+        } catch (error) { // eslint-disable-next-line no-console
+          console.error(error);
+        }
       },
-      editEmployee(id, updatedEmployee) {
-        this.employees = this.employees.map(employee =>
-          employee.id === id ? updatedEmployee : employee
-        )
+      async deleteEmployee(id) {
+        try {
+          await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'DELETE'
+          });
+          this.employees = this.employees.filter(
+            employee => employee.id !== id
+          )
+        } catch (error) { // eslint-disable-next-line no-console
+          console.error(error);
+        }
+      },
+      async editEmployee(id, updatedEmployee) {
+        try {
+          const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(updatedEmployee),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' },
+          })
+          const data = await response.json()
+          this.employees = this.employees.map(employee =>
+            employee.id === id ? data : employee
+          )
+        } catch (error) { // eslint-disable-next-line no-console
+          console.error(error);
+        }
       }
     }
   }
